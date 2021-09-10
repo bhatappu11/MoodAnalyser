d39f88deefa93a297f5ac8f7cd43f833486fd6cd
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-10 22:27:48 +0530

    ADDED: throws custom exception to handle both null and empty case

--- MoodAnalyser/src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ MoodAnalyser/src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -5,32 +5,51 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
 	@Test
-	public void givenMessage_WhenSad_ShouldReturnSad(){
+	public void givenMessage_WhenSad_ShouldReturnSad() throws Exception{
 		MoodAnalyser moodAnalyzer = new MoodAnalyser("I am in sad mood");
-		String mood = moodAnalyzer.analyseMood();
-		Assert.assertEquals("SAD",mood);
+		String mood = null ;
+		try {
+			mood = moodAnalyzer.analyseMood();
+			Assert.assertEquals("SAD",mood);
+		} catch (MoodAnalysisException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
 	@Test
-	public void givenMessage_WhenNotSad_ShouldReturnHappy() {
+	public void givenMessage_WhenNotSad_ShouldReturnHappy() throws Exception {
 		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("HAPPY", mood);
+		String mood = null;
+		try {
+			mood = moodAnalyser.analyseMood();
+			Assert.assertEquals("HAPPY", mood);
+		} catch (MoodAnalysisException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Test
+	public void givenMessage_WhenPassedNull_ShouldReturnHappy() throws MoodAnalysisException{
+		MoodAnalyser moodAnalyser = new MoodAnalyser("");
+		try {
+			moodAnalyser.analyseMood();
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY, e.type);
+			System.out.println(e.getMessage());
+		}
 	}
 	
 	@Test
-	public void givenMessage_WhenPassedNothing_ShouldReturnHappy() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser();
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("HAPPY", mood);
-	}
-	
-	@Test
-	public void givenMessage_WhenPassedNull_ShouldReturnHappy() {
+	public void givenMessage_WhenPassedEmpty_ShouldReturnHappy() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("HAPPY", mood);
+		try {
+			moodAnalyser.analyseMood();
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL, e.type);
+			System.out.println(e.getMessage());
+		}
 	}
 	
 }
