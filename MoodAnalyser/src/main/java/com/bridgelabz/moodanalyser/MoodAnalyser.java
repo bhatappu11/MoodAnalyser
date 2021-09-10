fa87a2ecb7ff89b552ac99eb542283f4f94b40e5
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-10 21:39:48 +0530

    MODIFIED: message is passed as parameter to constructor

--- MoodAnalyser/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ MoodAnalyser/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,7 +1,14 @@
 package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
-	public String analyseMood(String message) {
+	private String message;
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+	public MoodAnalyser() {
+		this.message = "";
+	}
+	public String analyseMood() {
 		if(message.contains("sad")) {
 			return "SAD";
 		}
--- MoodAnalyser/src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ MoodAnalyser/src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -6,15 +6,23 @@ import org.junit.Test;
 public class MoodAnalyserTest {
 	@Test
 	public void givenMessage_WhenSad_ShouldReturnSad(){
-		MoodAnalyser moodAnalyzer = new MoodAnalyser();
-		String mood = moodAnalyzer.analyseMood("This is a sad message");
+		MoodAnalyser moodAnalyzer = new MoodAnalyser("This is a sad message");
+		String mood = moodAnalyzer.analyseMood();
 		Assert.assertEquals("SAD",mood);
 	}
 	
+	
 	@Test
 	public void givenMessage_WhenNotSad_ShouldReturnHappy() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("This is a happy message");
+		String mood = moodAnalyser.analyseMood();
+		Assert.assertEquals("HAPPY", mood);
+	}
+	
+	@Test
+	public void givenMessage_WhenPassedNothing_ShouldReturnHappy() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
-		String mood = moodAnalyser.analyseMood("This is a happy message");
+		String mood = moodAnalyser.analyseMood();
 		Assert.assertEquals("HAPPY", mood);
 	}
 	
