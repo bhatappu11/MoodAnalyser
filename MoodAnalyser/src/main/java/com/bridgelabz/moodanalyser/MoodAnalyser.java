2c7a4857d639bf372aac8cfe0f0f8fa2e61e3f10
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-10 22:23:06 +0530

    MODIFIED: Used enum to differentiate the Mood analysis errors

--- MoodAnalyser/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ MoodAnalyser/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -2,21 +2,30 @@ package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
 	private String message;
+	
 	public MoodAnalyser(String message) {
 		this.message = message;
 	}
-	public MoodAnalyser() {
-		this.message = "";
-	}
-	public String analyseMood() {
+	
+	public String analyseMood(String message) throws MoodAnalysisException {
+		this.message = message;
 		try {
+			return analyseMood();
+		} catch (MoodAnalysisException e) {
+			throw e;
+		}
+	}
+	
+	public String analyseMood() throws MoodAnalysisException {
+		try {
+			if(message.length() == 0) throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,"Please don't send empty string");
 			if(message.contains("sad")) {
 				return "SAD";
 			}
 			else
 				return "HAPPY";
 		} catch(NullPointerException e) {
-			return "HAPPY";
+			throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL,"Please don't send null values");
 		}
 	}
 }
--- MoodAnalyser/src/main/java/com/bridgelabz/moodanalyser/MoodAnalysisException.java
+++ MoodAnalyser/src/main/java/com/bridgelabz/moodanalyser/MoodAnalysisException.java
@@ -1,7 +1,11 @@
 package com.bridgelabz.moodanalyser;
 
 public class MoodAnalysisException extends Exception {
-	public MoodAnalysisException(String message) {
-		super(message);
+	enum ExceptionType{
+		ENTERED_NULL,ENTERED_EMPTY
 	}
-}
+	ExceptionType type;
+	public MoodAnalysisException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}}
